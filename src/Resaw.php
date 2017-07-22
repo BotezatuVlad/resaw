af35f61de4b137f76d885fc5dfe67280f59e84d7
Vlad Botezatu <botezatu.vlad@yahoo.com>
2017-07-22 16:48:37 +0300

    - add composer.json to enable integration with packagist.org - add .gitignore file - create the resaw.php

---
+++ src/Resaw.php
@@ -0,0 +1,41 @@
+<?php
+
+/*
+ * (c) Vlad Botezatu <botezatu.vlad@yahoo.com>
+ *
+ * This source file is subject to the GNU GPLv3 license that is bundled
+ * with this source code in the file LICENSE.
+ */
+
+namespace freshcode\Resaw;
+
+class Resaw
+{
+	#-----------------------------------------
+	# magic happens
+	#-----------------------------------------
+	public function __construct()
+	{
+		
+	}
+	
+	
+	#-----------------------------------------
+	# public methods
+	#-----------------------------------------
+	/**
+	 * Apply transformation to image
+	 * 
+	 * @return boolean
+	 */
+	public function run()
+	{
+		return true;
+	}
+	
+	
+	
+	#-----------------------------------------
+	# protected methods
+	#-----------------------------------------
+}
